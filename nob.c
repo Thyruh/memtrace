84c91635bee3aeb16bc9335ba8002b2a30cd1b6e
Thyruh <mebiysy@gmail.com>
2026-03-21 13:18:45 +0100

    Modified the build system to accept the --lib flag to build the static library out of the memtrace.c file.

--- nob.c
+++ nob.c
@@ -14,6 +14,17 @@ int main(int argc, char** argv) {
    Nob_Cmd cmd = {0};
    nob_cmd_append(&cmd, CXX, "-o", DESTINATION"main", SRC"main.c", SRC"memtrace.c", CFLAGS);
    if (!nob_cmd_run(&cmd)) return 1;
+
+   if (argc >= 2 && strcmp(argv[argc-1], "--lib") == 0) { // Look into how strcmp looks like
+      nob_cmd_append(&cmd, CXX, "-c", SRC"memtrace.c", CFLAGS);
+      if (!nob_cmd_run(&cmd)) return 1;
+
+      nob_cmd_append(&cmd, "ar", "rcs", DESTINATION"libmemtrace.a", "memtrace.o");
+      if (!nob_cmd_run(&cmd)) return 1;
+
+      nob_cmd_append(&cmd, "rm", "memtrace.o");
+      if (!nob_cmd_run(&cmd)) return 1;
+   }
    return 0;
 }
 
